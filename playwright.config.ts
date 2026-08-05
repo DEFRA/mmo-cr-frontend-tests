d7c2b7dfd9c24e18fab8ae488263307662d42acb
Randhir Patel <randhir.patel@capgemini.com>
2026-08-05 19:56:23 +0530

    Integrate Allure reporting framework into Playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   ...(process.env.CI ? { workers: 1 } : {}),
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'allure-playwright',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL,
